00cc748975cac4961133dac5101da842af5b28d8
Michael Lannin <mlannin23@gmail.com>
2014-09-26 18:49:01 -0700

    unitTests

--- server/api/TESTAPI/index.js
+++ server/api/TESTAPI/index.js
@@ -6,5 +6,6 @@ var controller = require('./TESTAPI.controller');
 var router = express.Router();
 
 router.post('/resetFixture', controller.resetFixture);
+router.post('/unitTests', controller.unitTests);
 
 module.exports = router;
